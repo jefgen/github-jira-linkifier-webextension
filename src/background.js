0de893f03723f1db0f78bc7069c12c0c0013d390
Jeff Genovy <Jeff.Genovy@microsoft.com>
2018-08-12 01:32:14 -0700

    const all the things

--- src/background.js
+++ src/background.js
@@ -2,7 +2,7 @@
 "use strict";
 
 (function () {
-
+    // Set to true to enable logging output for debugging.
     const debugOutput = false;
 
     function logger(param) {
@@ -18,30 +18,30 @@
     }
 
     function modifyPage(storedSettings) {
-        if (!storedSettings.extensionSettings) {
+        if (!storedSettings || !storedSettings.extensionSettings) {
             logger("Extension not configured yet.");
             return;
         }
         
         const jiraIssueRegex = /([A-Z]{2,10}\-[0-9]+)+/g;
-        var openInNewTab = storedSettings.extensionSettings.useNewTab ? "target=\"_blank\"" : "";
-        var protocol = storedSettings.extensionSettings.useHttps ? "https://" : "http://";
-        var jiraUrlToUse = protocol + storedSettings.extensionSettings.jiraUrl;
+        const openInNewTab = storedSettings.extensionSettings.useNewTab ? "target=\"_blank\"" : "";
+        const protocol = storedSettings.extensionSettings.useHttps ? "https://" : "http://";
+        const jiraUrlToUse = protocol + storedSettings.extensionSettings.jiraUrl;
         logger("Jira URL: " + jiraUrlToUse);
 
         // GitHub can change the contents of the page without reloading, so
         // we need to periodically run this...
         setInterval(function () { 
             // Pull-Request listing page.
-            var pullRequests = document.getElementsByClassName('js-issue-row');
+            const pullRequests = document.getElementsByClassName('js-issue-row');
             if (pullRequests) {
                 logger("Found " + pullRequests.length + " pull-request(s).");
                 for (let pr of pullRequests) {
                     if (unlinked(pr, jiraUrlToUse)) {
                         // Find the original PR link.
-                        var prLinks = pr.getElementsByClassName('js-navigation-open');
-                        if (prLinks.length == 1) {
-                            var originalPrLinkObj = prLinks[0];
+                        const prLinks = pr.getElementsByClassName('js-navigation-open');
+                        if (prLinks && prLinks.length == 1) {
+                            const originalPrLinkObj = prLinks[0];
                             // We want the JIRA links to stand out visually from the PR link.
                             var jiraLinkClass = originalPrLinkObj.className.replace('js-navigation-open', '');
                             jiraLinkClass = jiraLinkClass.replace('link-gray-dark', '');
@@ -51,7 +51,7 @@
                                 <a href=\"" + originalPrLinkObj.getAttribute('href') + "\" class=\"" + originalPrLinkObj.className +"\">");
                             
                             // Fix up the nested link elements caused by the above replace.
-                            var newPrLinks = pr.getElementsByClassName('js-navigation-open');
+                            const newPrLinks = pr.getElementsByClassName('js-navigation-open');
                             if (newPrLinks == 2) {
                                 newPrlinks[0].remove(); // remove the now empty first one.
                             }
@@ -61,40 +61,40 @@
             }
 
             // Individual PR details page.
-            var detailsPageTitle = document.getElementsByClassName('js-issue-title');
+            const detailsPageTitle = document.getElementsByClassName('js-issue-title');
             if (detailsPageTitle) {
                 logger("Found " + detailsPageTitle.length + " pr title(s).");
                 if (detailsPageTitle.length == 1) {
-                    var detailsPageTitleObj = detailsPageTitle[0];
+                    const detailsPageTitleObj = detailsPageTitle[0];
                     if (unlinked(detailsPageTitleObj, jiraUrlToUse)) {
                         detailsPageTitleObj.innerHTML = detailsPageTitleObj.innerHTML.replace(jiraIssueRegex,
                             "<a " + openInNewTab + " href=\"" + jiraUrlToUse + "/browse/$&\">$&</a>");
                     }
                 }
             }
-            
+
             // Commits
-            var commitListing = document.getElementsByClassName('commit-title');
+            const commitListing = document.getElementsByClassName('commit-title');
             if (commitListing) {
                 logger("Found " + commitListing.length + " commit(s).");
                 for (let commitEntry of commitListing) {
                     if (unlinked(commitEntry, jiraUrlToUse)) {
                         // Check the type of commit page.
-                        var commitLinks = commitEntry.getElementsByClassName('message');
-                        if (commitLinks.length > 0) {
+                        const commitLinks = commitEntry.getElementsByClassName('message');
+                        if (commitLinks && commitLinks.length > 0) {
                             // Commit history page, with multiple commit titles.
 
                             // Get the original commit link(s).
                             // (There might be more than one, if the PR# was part of the commit title.)
-                            var originalCommitLinkObj = commitLinks[0];
+                            const originalCommitLinkObj = commitLinks[0];
                             // Look for any JIRA issues in the commit title.
-                            var issuesReferenced = originalCommitLinkObj.textContent.match(jiraIssueRegex);
+                            const issuesReferenced = originalCommitLinkObj.textContent.match(jiraIssueRegex);
                             if (issuesReferenced && issuesReferenced.length > 0) {
                                 logger(issuesReferenced);
                                 // Create links for each JIRA issue.
-                                var issues = new Array();
+                                const issues = new Array();
                                 for (let jiraIssue of issuesReferenced) {
-                                    var jiraCommitLink = document.createElement('a');
+                                    const jiraCommitLink = document.createElement('a');
                                     jiraCommitLink.innerHTML = jiraIssue + ' ';
                                     jiraCommitLink.setAttribute('href', jiraUrlToUse + "/browse/" + jiraIssue);
                                     jiraCommitLink.setAttribute('title', jiraIssue);
@@ -107,7 +107,7 @@
                                 originalCommitLinkObj.title = originalCommitLinkObj.title.replace(jiraIssueRegex, '');
                                 originalCommitLinkObj.textContent = originalCommitLinkObj.textContent.replace(jiraIssueRegex, '');
                                 // Now insert the new JIRA link(s) before the real commit link.
-                                var originalCommitLinkObjParent = originalCommitLinkObj.parentElement;
+                                const originalCommitLinkObjParent = originalCommitLinkObj.parentElement;
                                 for (var i=0; i<issues.length; i++) {
                                     originalCommitLinkObjParent.insertBefore(issues[i], originalCommitLinkObj);
                                 }
